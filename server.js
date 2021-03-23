ae71837515e2defc4d68163484464c9d53bcc743
Allin RAMANAMPISOA <Aallin13ramanampisoa@outlook.fr>
2021-03-23 09:31:12 +0300

    Enregistrer

--- server.js
+++ server.js
@@ -4,25 +4,27 @@ const fs = require('fs')
 const result = excelToJson({
     sourceFile: 'file.xlsx',
     columnToKey: {
-        A: 'nom',
-        B: 'quantite',
-        C: 'pu'
+        B: 'designation',
+        C: 'reference',
+        D: 'code',
+        I: 'quantite',
+        J: 'prixUnitaire'
     }
 });
 
-tmpProduct = result['Feuil1'];
+tmpProduct = result['Final'];
 
 var i = 0;
 var products = [];
 tmpProduct.forEach((ligne) =>{
-    if(i !== 0){
+    if(i !== 0 && i !== 1){
         products.push(ligne)
     }
     i++;
 })
 
 // convert JSON object to string
-const data = JSON.stringify(products);
+const data = JSON.stringify(products, null, 2);
 
 // write JSON string to a file
 fs.writeFile('produit.json', data, (err) => {
